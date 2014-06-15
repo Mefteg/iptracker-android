4ad50451192130eddb8d1e6b621f71b7b6bff424
Mefteg <gimenez.tom@gmail.com>
2014-06-15 17:37:45 +0200

    Save device name

--- iptracker/src/main/java/com/ladybug/iptracker_android/iptracker/MainActivity.java
+++ iptracker/src/main/java/com/ladybug/iptracker_android/iptracker/MainActivity.java
@@ -20,10 +20,12 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -39,6 +41,8 @@ public class MainActivity extends ActionBarActivity {
 
     private static final String FILENAME = "file_device_name";
 
+    String m_deviceName;
+
     private EditText m_editDeviceName;
     private Button m_btnSave;
     private Button m_btnPost;
@@ -50,35 +54,21 @@ public class MainActivity extends ActionBarActivity {
 
         m_editDeviceName = (EditText) findViewById(R.id.edit_device_name);
 
-        FileInputStream is = null;
-        try {
-            String deviceName = "";
-            byte[] buffer = new byte[1024];
-            is = openFileInput(FILENAME);
-            while (is.read(buffer, 0, 1024) > -1) {
-                deviceName += new String(buffer);
-            }
-            is.close();
-
-            if (deviceName.length() > 0) {
-                m_editDeviceName.setText(deviceName);
-            }
-        } catch (FileNotFoundException e) {
-            Log.e(TAG, e.toString());
-        } catch (IOException e) {
-            Log.e(TAG, e.toString());
+        m_deviceName = getDeviceName();
+        if (m_deviceName != null) {
+            m_editDeviceName.setText(m_deviceName);
         }
 
         m_btnSave = (Button) findViewById(R.id.btn_save);
         m_btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String deviceName = m_editDeviceName.getText();
-                Log.d(TAG, "Save: " + deviceName);
+                m_deviceName = m_editDeviceName.getText().toString();
+                Log.d(TAG, "Save: " + m_deviceName);
                 FileOutputStream fos = null;
                 try {
                     fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
-                    fos.write(deviceName.getBytes());
+                    fos.write(m_deviceName.getBytes());
                     fos.close();
                 } catch (FileNotFoundException e) {
                     e.printStackTrace();
@@ -99,29 +89,56 @@ public class MainActivity extends ActionBarActivity {
         });
     }
 
-    protected void postData() {
-        // Create a new HttpClient and Post Header
-        HttpClient httpclient = new DefaultHttpClient();
-        HttpPost httppost = new HttpPost(IPTRACKER_ADRESS + "/device/tom");
+    protected void postData(String _deviceName) {
+        if (_deviceName != null) {
+            // Create a new HttpClient and Post Header
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(IPTRACKER_ADRESS + "/device/" + _deviceName);
 
+            try {
+                // Add your data
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+                nameValuePairs.add(new BasicNameValuePair("id", "12345"));
+                nameValuePairs.add(new BasicNameValuePair("stringdata", "AndDev is Cool!"));
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                // Execute HTTP Post Request
+                HttpResponse response = httpclient.execute(httppost);
+
+                Log.d(TAG, response.toString());
+            } catch (ClientProtocolException e) {
+                // TODO Auto-generated catch block
+                Log.e(TAG, e.toString());
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                Log.e(TAG, e.toString());
+            }
+        }
+    }
+
+    protected String getDeviceName() {
+        String deviceName = null;
+
+        FileInputStream is = null;
         try {
-            // Add your data
-            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-            nameValuePairs.add(new BasicNameValuePair("id", "12345"));
-            nameValuePairs.add(new BasicNameValuePair("stringdata", "AndDev is Cool!"));
-            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            deviceName = "";
 
-            // Execute HTTP Post Request
-            HttpResponse response = httpclient.execute(httppost);
-
-            Log.d(TAG, response.toString());
-        } catch (ClientProtocolException e) {
-            // TODO Auto-generated catch block
+            is = openFileInput(FILENAME);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+            reader.close();
+            deviceName = sb.toString();
+        } catch (FileNotFoundException e) {
             Log.e(TAG, e.toString());
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             Log.e(TAG, e.toString());
         }
+
+        return deviceName;
     }
 
     @Override
@@ -146,7 +163,7 @@ public class MainActivity extends ActionBarActivity {
 
     private class DownloadFilesTask extends AsyncTask<URL, Integer, Long> {
         protected Long doInBackground(URL... urls) {
-            postData();
+            postData(m_deviceName);
 
             return new Long(0);
         }
